4224b17d39462c6a2194ab3f769017ed9c311933
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-09-15 13:11:48 +0200

    updating design of model edit

--- angular-api-master/src/app/components/post-edit/post-edit.component.ts
+++ angular-api-master/src/app/components/post-edit/post-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+} from '@angular/core';
 import { ApiClientService } from '../../services/api-client.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -10,24 +16,37 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
 })
-export class PostEditComponent {
-  @Input() post: any = {};
+export class PostEditComponent implements OnChanges {
+  @Input() post: any = {}; // Input post object
   @Output() postUpdated = new EventEmitter<void>();
   @Output() modalClosed = new EventEmitter<void>();
 
+  editedPost: any = {}; // Temporary object for form data
+  successMessage: string = ''; // Message after successful save
+
   constructor(private apiClient: ApiClientService) {}
 
+  ngOnChanges(): void {
+    // Make a copy of the input post to avoid direct modification
+    this.editedPost = { ...this.post };
+  }
+
   save(): void {
-    this.apiClient.updatePost(this.post.id, this.post).subscribe({
+    // Call API to update the post using the editedPost data
+    this.apiClient.updatePost(this.editedPost.id, this.editedPost).subscribe({
       next: () => {
-        this.postUpdated.emit(); 
-        this.close();
+        // Update the original post with editedPost values
+        Object.assign(this.post, this.editedPost);
+        this.successMessage = 'Post updated successfully!';
+        this.postUpdated.emit(); // Emit event to notify parent component
+        setTimeout(() => (this.successMessage = ''), 3000); // Clear message after 3 seconds
       },
       error: (err) => console.error('Error updating post', err),
     });
   }
 
   close(): void {
+    // Emit modalClosed event to close the modal without saving
     this.modalClosed.emit();
   }
 }
