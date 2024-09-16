5a16e49fdea85d2b12cf6a1d8984837eee3d37c7
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-09-16 11:50:04 +0200

    adding buttons for pagination

--- angular-api-master/src/app/components/pagination/pagination.component.ts
+++ angular-api-master/src/app/components/pagination/pagination.component.ts
@@ -1,16 +1,24 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css'],
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnChanges {
   @Input() currentPage = 1;
   @Input() totalPages = 1;
   @Output() pageChanged = new EventEmitter<number>();
+  
+  pages: number[] = [];
+
+  ngOnChanges(): void {
+    this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
 
   onPageChange(page: number): void {
-    this.pageChanged.emit(page);
+    if (page >= 1 && page <= this.totalPages) {
+      this.pageChanged.emit(page);
+    }
   }
 }
