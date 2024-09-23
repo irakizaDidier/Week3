6a3446a99655c6df7eca59c70946a7a1cc214d17
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-09-23 23:40:14 +0200

    working on edit and add tsk

--- kanban-task-management-web-app/src/app/components/single-task/single-task.component.ts
+++ kanban-task-management-web-app/src/app/components/single-task/single-task.component.ts
@@ -17,7 +17,6 @@ export class SingleTaskComponent implements OnInit {
     console.log('Received Task:', this.task);
   }
 
-  // Toggle the subtask completion status and dispatch the update action
   toggleSubtask(subtask: any): void {
     if (this.task) {
       const updatedSubtaskStatus = !subtask.isCompleted;
@@ -31,27 +30,23 @@ export class SingleTaskComponent implements OnInit {
     }
   }
 
-  // Update the task's status when the select input changes
   updateTaskStatus(): void {
     if (this.task) {
       this.store.dispatch(TaskActions.updateTask({ task: this.task }));
     }
   }
 
-  // Calculate the number of completed subtasks
   getCompletedSubtaskCount(): number {
     return (
       this.task?.subtasks.filter((subtask) => subtask.isCompleted).length || 0
     );
   }
 
-  // Close the modal and clear the task
   closeModal(): void {
     this.task = undefined;
     console.log('Modal closed');
   }
 
-  // Handle clicking outside the modal to close it
   onContainerClick(event: MouseEvent): void {
     this.closeModal();
   }
